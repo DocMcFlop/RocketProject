266e26f7afde4098d53ac9747232882b9b38ada6
adbnm4 <98531465+DocMcFlop@users.noreply.github.com>
2022-02-01 21:06:41 -0600

    Added dark theme to app

--- rocketgen.py
+++ rocketgen.py
@@ -6,21 +6,25 @@ from ttkthemes import ThemedStyle
 root = Tk()
 root.title('RocketGen')
 root.iconbitmap('icon.ico')
+style = ttk.Style(root)
+root.tk.call('lappend', 'auto_path', 'awthemes-10.4.0')
+root.tk.call('package', 'require', 'awdark')
+style.theme_use('awdark')
 my_img = ImageTk.PhotoImage(Image.open("RocketGenBanner.png"))
 
 # Notebooks
 my_notebook = ttk.Notebook(root)
 # Labels
-program_banner = Label(image=my_img, borderwidth=5)
+program_banner = ttk.Label(image=my_img)
 # Frames
-my_frame1 = Frame(my_notebook, width=1080, height=720)
-my_frame2 = Frame(my_notebook, width=1080, height=720)
-my_frame3 = Frame(my_notebook, width=1080, height=720)
-my_frame4 = Frame(my_notebook, width=1080, height=720)
-my_frame5 = Frame(my_notebook, width=1080, height=720)
-my_frame6 = Frame(my_notebook, width=1080, height=720)
-my_frame7 = Frame(my_notebook, width=1080, height=720)
-my_frame8 = Frame(my_notebook, width=1080, height=720)
+my_frame1 = ttk.Frame(my_notebook, width=1080, height=720)
+my_frame2 = ttk.Frame(my_notebook, width=1080, height=720)
+my_frame3 = ttk.Frame(my_notebook, width=1080, height=720)
+my_frame4 = ttk.Frame(my_notebook, width=1080, height=720)
+my_frame5 = ttk.Frame(my_notebook, width=1080, height=720)
+my_frame6 = ttk.Frame(my_notebook, width=1080, height=720)
+my_frame7 = ttk.Frame(my_notebook, width=1080, height=720)
+my_frame8 = ttk.Frame(my_notebook, width=1080, height=720)
 
 # Pack widgets
 program_banner.grid(row=0, column=0)
@@ -43,9 +47,6 @@ my_notebook.add(my_frame5, text="Injector")
 my_notebook.add(my_frame6, text="Heat Transfer")
 my_notebook.add(my_frame7, text="Stress Analysis")
 my_notebook.add(my_frame8, text="CEA Evaluation")
-
-
-
 
 
 
